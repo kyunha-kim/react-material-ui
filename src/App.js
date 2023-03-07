de61436022f10b357f6ce092879221b3f4734dc6
kim-seonkyu <seonkyu93@gmail.com>
2023-03-07 22:51:51 +0900

    Button / Icons / emotion-styled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import { Accessibility, AcUnit } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button startIcon={<AcUnit />} fullWidth size="large" variant="contained">
+        Contained
+      </Button>
+      <Button endIcon={<Accessibility />} variant="outlined">
+        Outlined
+      </Button>
+      <Button
+        sx={{
+          backgroundColor: "gray",
+          color: "#fff",
+          margin: "10px",
+          "&:hover": {
+            backgroundColor: "#000",
+          },
+          "&:disabled": {
+            backgroundColor: "#fff",
+            color: "#000",
+          },
+        }}
+      >
+        Custom Style
+      </Button>
+      <CustomButton>CustomButton</CustomButton>
+
+      <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
     </div>
   );
 }
+
+const CustomButton = styled(Button)({
+  backgroundColor: "gray",
+  color: "#fff",
+  margin: "10px",
+  "&:hover": {
+    backgroundColor: "#000",
+  },
+  "&:disabled": {
+    backgroundColor: "#fff",
+    color: "#000",
+  },
+});
 
 export default App;
